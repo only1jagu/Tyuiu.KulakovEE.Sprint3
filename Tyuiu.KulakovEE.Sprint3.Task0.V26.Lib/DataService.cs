c08ad37a41ae7afc4bfff78d0fb016daf9438e92
Егор Кулаков <egorkakylakov2000@mail.ru>
2023-10-20 18:27:57 +0500

    Add project files.

---
+++ Tyuiu.KulakovEE.Sprint3.Task0.V26.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KulakovEE.Sprint3.Task0.V26.Lib
+{
+        public class DataService : ISprint3Task0V26
+        {
+            public double GetMultiplySeries(int startValue, int stopValue)
+            {
+                double series = 1;
+                int i;
+                for (i = startValue; i <= stopValue; i++)
+                {
+                    series = series * (((Math.Pow(2, i)) / (i + 1)) * Math.Cos(1.8));
+                }
+                return Math.Round(series, 3);
+            }
+        }
+    
+
+}
+
+
---
+++ Tyuiu.KulakovEE.Sprint3.Task0.V26/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KulakovEE.Sprint3.Task0.V26.Lib;
+
+namespace Tyuiu.KulakovEE.Sprint3.Task0.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Кулаков Е.Е. | АСОиУб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Выполнил: Кулаков Е.Е. | АСОиУб-23-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение   *");
+            Console.WriteLine("* ряда по формуле                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int startValue = 1;
+            int stopValue = 9;
+
+
+            Console.WriteLine("Start Value = " + startValue);
+            Console.WriteLine("Stop Value = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
+        
+
